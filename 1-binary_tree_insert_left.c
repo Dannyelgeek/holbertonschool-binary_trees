d4a6b14a30c49b38b9a9ecd6dde82d5af0f17782
Dannyelgeek <daot1993@gmail.com>
2023-01-17 16:21:23 -0500

    binary trees task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 
-	if (!parent)
+	if (!parent || !new_node)
 		return (NULL);
 	
 	new_node->n = value;
